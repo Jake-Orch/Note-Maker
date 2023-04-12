7c6848384a3aa47e4b15b90910214db0fe3e70b2
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-04-12 13:25:33 +0100

    Note save problem solved

--- routes/notes.js
+++ routes/notes.js
@@ -8,13 +8,13 @@ const fs = require('fs');
 notesRoute.post('/', (req, res) => {
     console.info(`${req.method} request received to add a note`);
     console.info(`${req.body}`);
-    const { noteTitle, noteBody } = req.body;
-    console.info(`${noteTitle} ${noteBody}`)
-    if (noteTitle && noteBody) {
+    const { title, text } = req.body;
+    console.info(`${title} ${text}`)
+    if (title && text) {
       console.info(`req.body working`)
         const newNote = {
             title,
-            note
+            text
             // ADD ID MAKER           note_id: uuid()
         };
   
@@ -29,7 +29,7 @@ notesRoute.post('/', (req, res) => {
   
                 console.log('so far so good')
                 fs.writeFile('./db/db.json', 
-                JSON.stringify(parsedData, null, 3),
+                JSON.stringify(parsedData),
                 (writeE) =>
                 writeE
                 ? console.error(writeE)
@@ -40,53 +40,6 @@ notesRoute.post('/', (req, res) => {
     }
   });
 
-  // notesRoute.post('/', (req, res) => {
-  //   console.info(`${req.method} request received to add a note`);
-  
-  //   const { title, note } = req.body;
-  
-  //   // If all the required properties are present
-  //   if (title, note) {
-  //     // Variable for the object we will save
-  //     const newNote = {
-  //       title,
-  //       note
-  //       // ADD id 
-  //     };
-  
-  //     // Obtain existing reviews
-  //     fs.readFile('./db/db.json', 'utf8', (err, data) => {
-  //       if (err) {
-  //         console.error(err);
-  //       } else {
-  //         const parsedNotes = JSON.parse(data);
-  
-  //         // Add a new review
-  //         parsedNotes.push(newNote);
-  
-  //         // Write updated reviews back to the file
-  //         fs.writeFile(
-  //           './db/reviews.json',
-  //           JSON.stringify(parsedNotes, null, 4),
-  //           (writeErr) =>
-  //             writeErr
-  //               ? console.error(writeErr)
-  //               : console.info('Successfully updated reviews!')
-  //         );
-  //       }
-  //     });
-  
-  //     const response = {
-  //       status: 'success',
-  //       body: newNote,
-  //     };
-  
-  //     console.log(response);
-  //     res.status(201).json(response);
-  //   } else {
-  //     res.status(500).json('Error in posting review');
-  //   }
-  // });
   
 
 module.exports = notesRoute;
