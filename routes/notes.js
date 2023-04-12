cdc2d24ab7e6f6f1c43635fc1286787191e17118
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-04-12 15:45:38 +0100

    unique id added to new notes, uuid added

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const notesRoute = express.Router();
 const fs = require('fs');
-const { uuid } = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
 
 // router.put('/notes') etc.
 
@@ -19,24 +19,19 @@ notesRoute.get('/', (req, res) => {
 
 notesRoute.post('/', (req, res) => {
   console.info(`${req.method} request received to add a note`);
-  console.info(`${req.body}`);
   const { title, text } = req.body;
-  console.info(`${title} ${text}`)
   if (title && text) {
     const newNote = {
       title,
-      text
-      // ADD ID MARKER           note_id: uuid()
+      text,
+      note_id: uuidv4(),
     };
     fs.readFile('./db/db.json', 'utf8', (e, d) => {
       if (e) {
         console.error(e);
       } else {
         const parsedData = JSON.parse(d);
-
         parsedData.push(newNote);
-
-        console.log('so far so good')
         fs.writeFile('./db/db.json',
           JSON.stringify(parsedData, null, 3),
           (writeE) =>
@@ -47,6 +42,7 @@ notesRoute.post('/', (req, res) => {
       }
     });
   }
+  location.reload()
 });
 
 
