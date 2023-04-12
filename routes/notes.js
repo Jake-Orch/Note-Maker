954103bb4dcf4ab0366d2fe644934e65d32a3dac
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-04-12 15:21:58 +0100

    Displaying notes fixed

--- routes/notes.js
+++ routes/notes.js
@@ -9,50 +9,45 @@ notesRoute.get('/', (req, res) => {
   fs.readFile('./db/db.json', 'utf8', (e, d) => {
     if (e) {
       console.error(e);
-  } else {
-    const notes = JSON.parse(d)
-    // console.info(notes);
-    // for (let i = 0; i < parsedData.length; i++) {
-    //   const currentNote = parsedData[i];
-    //   { noteTitle, noteText}
-    // }
-    return notes
-  }
+    } else {
+      const notes = JSON.parse(d)
+      return res.json(notes)
+    }
   })
 });
 
 notesRoute.post('/', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
-    console.info(`${req.body}`);
-    const { title, text } = req.body;
-    console.info(`${title} ${text}`)
-    if (title && text) {
-        const newNote = {
-            title,
-            text
-            // ADD ID MARKER           note_id: uuid()
-        };
-        fs.readFile('./db/db.json', 'utf8', (e, d) => {
-            if (e) {
-                console.error(e);
-            } else {
-                const parsedData = JSON.parse(d);
-  
-                parsedData.push(newNote);
-  
-                console.log('so far so good')
-                fs.writeFile('./db/db.json', 
-                JSON.stringify(parsedData, null, 3),
-                (writeE) =>
-                writeE
-                ? console.error(writeE)
-                : console.info('Note added successfully!')
-                );
-            }
-        });
-    }
-  });
+  console.info(`${req.method} request received to add a note`);
+  console.info(`${req.body}`);
+  const { title, text } = req.body;
+  console.info(`${title} ${text}`)
+  if (title && text) {
+    const newNote = {
+      title,
+      text
+      // ADD ID MARKER           note_id: uuid()
+    };
+    fs.readFile('./db/db.json', 'utf8', (e, d) => {
+      if (e) {
+        console.error(e);
+      } else {
+        const parsedData = JSON.parse(d);
 
-  
+        parsedData.push(newNote);
+
+        console.log('so far so good')
+        fs.writeFile('./db/db.json',
+          JSON.stringify(parsedData, null, 3),
+          (writeE) =>
+            writeE
+              ? console.error(writeE)
+              : console.info('Note added successfully!')
+        );
+      }
+    });
+  }
+});
+
+
 
 module.exports = notesRoute;
